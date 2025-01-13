34f2670c521fda37e54fa601807fa6883747163b
Dor Bar <dorbar424@gmail.comgit config --global user.name Dor>
2025-01-13 23:02:09 +0200

    amit

---
+++ dor.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let movie ={ 
+ name: `amit0`,
+ time :"10 min"
+}
+
+app.get('/', (req, res) => {
+  res.send(movie)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
